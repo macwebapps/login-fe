bac2ca350447850c536d00563eff3d79060ba4f4
Francis Macugay <francis.macugay@icloud.com>
2017-04-23 07:23:34 +0800

    Angular 2 implementation of front-end login/registration.

---
+++ src/login/auth.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+ 
+@Injectable()
+export class AuthService {
+
+    constructor(private http: Http) { }
+ 
+    login(username: string, password: string) {
+        let headers = new Headers();
+        headers.append('Authorization', "Basic " + btoa(username + ":" + password));
+
+        let options = new RequestOptions({ headers: headers });
+
+        return this.http.get('http://localhost:8090/login/'+username, options);
+        //     .map((response: Response) => {
+        //         // login successful if there's a jwt token in the response
+        //         let user = response.json();
+        //         if (user) {
+        //             // store user details and jwt token in local storage to keep user logged in between page refreshes
+        //             localStorage.setItem('currentUser', JSON.stringify(user));
+        //         }
+        //     });
+    }
+
+    // login(username: string, password: string) {
+    //     return this.http.post('/api/authenticate', JSON.stringify({ username: username, password: password }))
+    //         .map((response: Response) => {
+    //             // login successful if there's a jwt token in the response
+    //             let user = response.json();
+    //             if (user && user.token) {
+    //                 // store user details and jwt token in local storage to keep user logged in between page refreshes
+    //                 localStorage.setItem('currentUser', JSON.stringify(user));
+    //             }
+    //         });
+    // }
+ 
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+    }
+}
