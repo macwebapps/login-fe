2655f77618d131ea10dc773cf9fb337e53cfcdc6
Froilan Macugay <froilan.macugay@pictureworks.com.au>
2017-05-01 15:13:56 +1000

    change backend endpoint

--- src/login/auth.service.ts
+++ src/login/auth.service.ts
@@ -2,23 +2,22 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
- 
+
 @Injectable()
 export class AuthService {
 
     constructor(private http: Http) { }
- 
+
     login(username: string, password: string) {
-        let headers = new Headers();
-        headers.append('Authorization', "Basic " + btoa(username + ":" + password));
 
-        let options = new RequestOptions({ headers: headers });
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+      let options = new RequestOptions({ headers: headers });
 
-        console.log(options);
-        return this.http.get('http://localhost:8090/login/'+username, options)
+        return this.http.post('http://localhost:8090/login', JSON.stringify({ username: username, password: password }),  options)
              .map((response: Response) => {
                  // login successful if there's a jwt token in the response
                  let user = response.json();
+                 console.log(user);
                  if (user) {
                      // store user details and jwt token in local storage to keep user logged in between page refreshes
                      localStorage.setItem('currentUser', JSON.stringify(user));
--- test/hello.js
+++ test/hello.js
@@ -3,14 +3,15 @@
  */
 $(document).ready(function() {
   $.ajax({
-    url: "http://localhost:8090/login/user",
-    type: 'GET',
+    url: "http://localhost:8090/login",
+    type: 'POST',
     headers: {
-      'Authorization':'Basic dXNlcjp1c2Vy'
+      'Content-Type': 'application/json'
 
       //'X_CSRF_TOKEN':'xxxxxxxxxxxxxxxxxxxx',
       //'Content-Type':'application/json'
-    }
+    },
+    data : JSON.stringify({ "username": "user", "password" : "user" })
   }).then(function(data, status, jqxhr) {
     $('.cors-response').append(JSON.stringify(data));
     console.log(jqxhr);
